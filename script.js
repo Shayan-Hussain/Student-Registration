15e4ef0811cdafc040d74e582bb7a9bc424f91cb
fahad <fahadzardari111@gmail.com>
2023-08-05 01:19:25 +0500

    changes made data adding to mock json

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-// function add_to_json_file() {
-//     document.getElementById('submit_btn').addEventListener( 'submit' , 
-//         function(event) {
-//             let student_details = {
-//                 sr : 1,
-//                 name : document.getElementById('f_name').value + ' ' + document.getElementById('l_name').value,
-//                 age : Date.now() - getElementById('b_date').value,
-//                 enrol_date : Date.now()
-//             }
-//         } 
-//     )
-// }
+import students from "./student_data.json" assert { type: "json" };
 
+const generateRow = () => {
+  document.getElementById("table_body").innerHTML = "";
 
+  students.map((student, i) => {
+    const rowHtml = `<tr>
+                <td>${student.id}</td>
+                <td>${student.full_name}</td>
+                <td>${student.age}</td>
+                <td>${student.enrolled}</td>
+                </tr>
+            `;
+    document.getElementById("table_body").innerHTML += rowHtml;
+  });
+};
+generateRow();
 
-
-
-
-
-    // function add_new_row() {
-    //     let table = document.getElementById("table");
-    //     let new_row = document.createElement("tr");
-    //     let sr_cell = document.createElement("td");
-    //     let name_cell = document.createElement("td");
-    //     let b_date = document.createElement("td");
-    //     let date_enrolled = document.createElement("td");
-        
-    // }
-let full_date_today = new Date();
-
-function print() {
-    let f_name = document.getElementById('f_name').value;
-    let l_name = document.getElementById('l_name').value;
-
-    let student_details = {
-        sr : 1,
-        name : f_name + ' ' + l_name,
-        age : calculate_age(),
-        enrol_date : full_date_today.getDate() + '/' + (full_date_today.getMonth()+1) + '/' + full_date_today.getFullYear()
+const writeDataToFile = () => {
+  const data = JSON.stringify(students);
+  writeFile("student_data.json", data, (err) => {
+    if (err) {
+      throw err;
     }
-    alert(console.log(student_details))
+    console.log("JSON data is saved.");
+  });
+};
+
+let full_date_today = new Date();
+function insert() {
+  let f_name = document.getElementById("f_name").value;
+  let l_name = document.getElementById("l_name").value;
+
+  let student_details = {
+    id: 1,
+    full_name: f_name + " " + l_name,
+    age: calculate_age(),
+    enrolled:
+      full_date_today.getDate() +
+      "/" +
+      (full_date_today.getMonth() + 1) +
+      "/" +
+      full_date_today.getFullYear(),
+  };
+  students.push(student_details);
+  writeDataToFile();
+  generateRow();
 }
 
+document.getElementById("submit_btn").addEventListener("click", insert);
+
 function calculate_age() {
-
-    const birth_date_input = new Date(document.getElementById('b_date').value);
-
-    const month_of_birth = birth_date_input.getMonth();
-    const day_of_birth = birth_date_input.getDate();
-    const year_of_birth = birth_date_input.getFullYear();
-    
-    const years_old = full_date_today.getFullYear() - year_of_birth;
-
-    let age = years_old;
-
-    const month_diff = full_date_today.getMonth() - month_of_birth;
-    const date_diff = full_date_today.getDate() - day_of_birth;
-
-    if (month_diff <= 0 && date_diff < 0) {
-        age -= 1;
-    }
+  const birth_date_input = new Date(document.getElementById("b_date").value);
+  let years_old =
+    full_date_today.getFullYear() - birth_date_input.getFullYear();
+  const month_diff = full_date_today.getMonth() - birth_date_input.getMonth();
+  const date_diff = full_date_today.getDate() - birth_date_input.getDate();
+  if (month_diff < 0) {
+    years_old--;
+  } else if (month_diff === 0 && date_diff < 0) {
+    years_old--;
+  }
+  return years_old;
 }
